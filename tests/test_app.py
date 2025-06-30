1723fc594dc0dae05b4b7b7a3269dde514d4d2de
CarlosNunek <chnunez@uce.edu.ec>
2025-06-29 23:47:02 -0500

    fix: agregar sys.path para importar app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,7 @@
 import sys
 import os
 from app import app
-
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
 def test_home():
     client = app.test_client()
     response = client.get("/")  # O cualquier endpoint que tengas
