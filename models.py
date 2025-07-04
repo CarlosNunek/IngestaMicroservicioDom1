67571d247017b0dbdb40682b4c1a99a10b6abeab
CarlosNunek <chnunez@uce.edu.ec>
2025-07-04 15:16:33 -0500

    fix:Arreglo lineas en models.py v2

--- models.py
+++ models.py
@@ -16,4 +16,4 @@ def llamar_preprocesamiento(id_cedula):
         print("[✓] Preprocesamiento lanzado:", response.json())
     except Exception as e:
         print("[!] Error al llamar preprocesamiento:", str(e))
-                                                                                              
+                                                                                          
